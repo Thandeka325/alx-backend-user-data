0e7007cf3f731170892617c4815918a061518e5d
Thandeka325 <marhwayiza325@gmail.com>
2025-05-27 13:09:49 +0000

    Project: 0x01-Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,3 +46,25 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts the user email and password from a Base64 decoded
+        authorization header
+
+        Args:
+            decoded_base64_authorization_header (str): Decoded Base64 str
+
+        Returns:
+            tuple: (email, password) if valid, otherwise (None, None)
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
