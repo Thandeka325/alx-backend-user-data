a100964ccd63ecd76983656f59b8c30a9164cc94
Thandeka325 <marhwayiza325@gmail.com>
2025-05-28 12:57:04 +0000

    Project: 0x02-Session_authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module for session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """"
+    SessionAuth class inheriting from Auth.
+    """
+    pass
