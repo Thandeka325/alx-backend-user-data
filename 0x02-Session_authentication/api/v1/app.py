d0e5efe08cc9a74986dc5352b0c6057f5f2e0b5e
Thandeka325 <marhwayiza325@gmail.com>
2025-05-28 19:58:33 +0000

    Project: 0x02-Session_authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,10 +6,11 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -66,7 +67,8 @@ def before_request_func():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/'
+            '/api/v1/auth_session/login/',
+            '/api/v1/auth_session/login'
     ]
     if not auth.require_auth(request.path, excluded_paths):
         return
